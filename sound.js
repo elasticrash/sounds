4cb2bd5bd11829dbccbf803e2252f6297a8b9542
stefano kitano <S.Kouroupis@gmail.com>
2016-04-12 23:46:21 +0100

    Initial commit

---
+++ sound.js
@@ -0,0 +1,685 @@
+/**
+ * Created by tougo on 12/4/16.
+ */
+
+var notes = [{
+    name: 'C',
+    altname: null,
+    position: 0,
+    frequency: 16.35,
+    wavelength: 2109.89
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 0,
+    frequency: 17.32,
+    wavelength: 1991.47
+}, {
+    name: 'D',
+    altname: null,
+    position: 0,
+    frequency: 18.35,
+    wavelength: 1879.69
+}, {
+    name: 'D#',
+    altname: 'Eb',
+    position: 0,
+    frequency: 19.45,
+    wavelength: 1774.20
+}, {
+    name: 'E',
+    altname: null,
+    position: 0,
+    frequency: 20.60,
+    wavelength: 1674.62
+}, {
+    name: 'F',
+    altname: null,
+    position: 0,
+    frequency: 21.83,
+    wavelength: 1580.63
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 0,
+    frequency: 23.12,
+    wavelength: 1491.91
+}, {
+    name: 'G',
+    altname: null,
+    position: 0,
+    frequency: 24.50,
+    wavelength: 1408.18
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 0,
+    frequency: 25.96,
+    wavelength: 1329.14
+}, {
+    name: 'A',
+    altname: null,
+    position: 0,
+    frequency: 27.50,
+    wavelength: 1254.55
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 0,
+    frequency: 29.14,
+    wavelength: 1184.13
+}, {
+    name: 'B',
+    altname: null,
+    position: 0,
+    frequency: 30.87,
+    wavelength: 1117.67
+}, {
+    name: 'C',
+    altname: null,
+    position: 1,
+    frequency: 32.70,
+    wavelength: 1054.94
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 1,
+    frequency: 34.65,
+    wavelength: 995.73
+}, {
+    name: 'D',
+    position: 1,
+    frequency: 36.71,
+    wavelength: 939.85
+}, {
+    name: 'D#',
+    altname: 'Eb',
+    position: 1,
+    frequency: 38.89,
+    wavelength: 887.10
+}, {
+    name: 'E',
+    altname: null,
+    position: 1,
+    frequency: 41.20,
+    wavelength: 837.31
+}, {
+    name: 'F',
+    altname: null,
+    position: 1,
+    frequency: 43.65,
+    wavelength: 790.31
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 1,
+    frequency: 46.25,
+    wavelength: 745.96
+}, {
+    name: 'G',
+    altname: null,
+    position: 1,
+    frequency: 49.00,
+    wavelength: 704.09
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 1,
+    frequency: 51.91,
+    wavelength: 664.57
+}, {
+    name: 'A',
+    altname: null,
+    position: 1,
+    frequency: 55.00,
+    wavelength: 627.27
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 1,
+    frequency: 58.27,
+    wavelength: 592.07
+}, {
+    name: 'B',
+    altname: null,
+    position: 1,
+    frequency: 61.74,
+    wavelength: 558.84
+}, {
+    name: 'C',
+    altname: null,
+    position: 2,
+    frequency: 65.41,
+    wavelength: 527.47
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 2,
+    frequency: 69.30,
+    wavelength: 497.87
+}, {
+    name: 'D',
+    altname: null,
+    position: 2,
+    frequency: 73.42,
+    wavelength: 469.92
+}, {
+    name: 'D#',
+    altname: 'Eb',
+    position: 2,
+    frequency: 77.78,
+    wavelength: 443.55
+}, {
+    name: 'E',
+    altname: null,
+    position: 2,
+    frequency: 82.41,
+    wavelength: 418.65
+}, {
+    name: 'F',
+    altname: null,
+    position: 2,
+    frequency: 87.31,
+    wavelength: 395.16
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 2,
+    frequency: 92.50,
+    wavelength: 372.98
+}, {
+    name: 'G',
+    altname: null,
+    position: 2,
+    frequency: 98.00,
+    wavelength: 352.04
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 2,
+    frequency: 103.83,
+    wavelength: 332.29
+}, {
+    name: 'A',
+    altname: null,
+    position: 2,
+    frequency: 110.00,
+    wavelength: 313.64
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 2,
+    frequency: 116.54,
+    wavelength: 296.03
+}, {
+    name: 'B',
+    altname: null,
+    position: 2,
+    frequency: 123.47,
+    wavelength: 279.42
+}, {
+    name: 'C',
+    altname: null,
+    position: 3,
+    frequency: 130.81,
+    wavelength: 263.74
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 3,
+    frequency: 138.59,
+    wavelength: 248.93
+}, {
+    name: 'D',
+    altname: null,
+    position: 3,
+    frequency: 146.83,
+    wavelength: 234.96
+}, {
+    name: 'D#',
+    altname: 'Eb',
+    position: 3,
+    frequency: 155.56,
+    wavelength: 221.77
+}, {
+    name: 'E',
+    altname: null,
+    position: 3,
+    frequency: 164.81,
+    wavelength: 209.33
+}, {
+    name: 'F',
+    altname: null,
+    position: 3,
+    frequency: 174.61,
+    wavelength: 197.58
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 3,
+    frequency: 185.00,
+    wavelength: 186.49
+}, {
+    name: 'G',
+    altname: null,
+    position: 3,
+    frequency: 196.00,
+    wavelength: 176.02
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 3,
+    frequency: 207.65,
+    wavelength: 166.14
+}, {
+    name: 'A',
+    altname: null,
+    position: 3,
+    frequency: 220.00,
+    wavelength: 156.82
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 3,
+    frequency: 233.08,
+    wavelength: 148.02
+}, {
+    name: 'B',
+    altname: null,
+    position: 3,
+    frequency: 246.94,
+    wavelength: 139.1
+}, {
+    name: 'C',
+    altname: null,
+    position: 4,
+    frequency: 261.63,
+    wavelength: 131.87
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 4,
+    frequency: 277.18,
+    wavelength: 124.47
+}, {
+    name: 'D',
+    altname: null,
+    position: 4,
+    frequency: 293.66,
+    wavelength: 117.48
+}, {
+    name: 'D#',
+    altname: 'Eb',
+    position: 4,
+    frequency: 311.13,
+    wavelength: 110.89
+}, {
+    name: 'E',
+    altname: null,
+    position: 4,
+    frequency: 329.63,
+    wavelength: 104.66
+}, {
+    name: 'F',
+    altname: null,
+    position: 4,
+    frequency: 349.23,
+    wavelength: 98.79
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 4,
+    frequency: 369.99,
+    wavelength: 93.24
+}, {
+    name: 'G',
+    altname: null,
+    position: 4,
+    frequency: 392.00,
+    wavelength: 88.01
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 4,
+    frequency: 415.30,
+    wavelength: 83.07
+}, {
+    name: 'A',
+    altname: null,
+    position: 4,
+    frequency: 440.00,
+    wavelength: 78.41
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 4,
+    frequency: 466.16,
+    wavelength: 74.01
+}, {
+    name: 'B',
+    altname: null,
+    position: 4,
+    frequency: 493.88,
+    wavelength: 69.85
+}, {
+    name: 'C',
+    altname: null,
+    position: 5,
+    frequency: 523.25,
+    wavelength: 65.93
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 5,
+    frequency: 554.37,
+    wavelength: 62.23
+}, {
+    name: 'D',
+    altname: null,
+    position: 5,
+    frequency: 587.33,
+    wavelength: 58.74
+}, {
+    name: 'D#',
+    altname: 'Eb',
+    position: 5,
+    frequency: 622.25,
+    wavelength: 55.44
+}, {
+    name: 'E',
+    altname: null,
+    position: 5,
+    frequency: 659.25,
+    wavelength: 52.33
+}, {
+    name: 'F',
+    altname: null,
+    position: 5,
+    frequency: 698.46,
+    wavelength: 49.39
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 5,
+    frequency: 739.99,
+    wavelength: 46.62
+}, {
+    name: 'G',
+    altname: null,
+    position: 5,
+    frequency: 783.99,
+    wavelength: 44.01
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 5,
+    frequency: 830.61,
+    wavelength: 41.54
+}, {
+    name: 'A',
+    altname: null,
+    position: 5,
+    frequency: 880.00,
+    wavelength: 39.20
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 5,
+    frequency: 932.33,
+    wavelength: 37.00
+}, {
+    name: 'B',
+    altname: null,
+    position: 5,
+    frequency: 987.77,
+    wavelength: 34.93
+}, {
+    name: 'C',
+    altname: null,
+    position: 6,
+    frequency: 1046.50,
+    wavelength: 32.97
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 6,
+    frequency: 1108.73,
+    wavelength: 31.12
+}, {
+    name: 'D',
+    altname: null,
+    position: 6,
+    frequency: 1174.66,
+    wavelength: 29.37
+}, {
+    name: 'D#',
+    altname: 'Eb6',
+    position: 6,
+    frequency: 1244.51,
+    wavelength: 27.72
+}, {
+    name: 'E',
+    altname: null,
+    position: 6,
+    frequency: 1318.51,
+    wavelength: 26.17
+}, {
+    name: 'F',
+    altname: null,
+    position: 6,
+    frequency: 1396.91,
+    wavelength: 24.70
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 6,
+    frequency: 1479.98,
+    wavelength: 23.31
+}, {
+    name: 'G',
+    altname: null,
+    position: 6,
+    frequency: 1567.98,
+    wavelength: 22.00
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 6,
+    frequency: 1661.22,
+    wavelength: 20.77
+}, {
+    name: 'A',
+    altname: null,
+    position: 6,
+    frequency: 1760.00,
+    wavelength: 19.60
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 6,
+    frequency: 1864.66,
+    wavelength: 18.50
+}, {
+    name: 'B',
+    altname: null,
+    position: 6,
+    frequency: 1975.53,
+    wavelength: 17.46
+}, {
+    name: 'C',
+    altname: null,
+    position: 7,
+    frequency: 2093.00,
+    wavelength: 16.48
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 7,
+    frequency: 2217.46,
+    wavelength: 15.56
+}, {
+    name: 'D',
+    altname: null,
+    position: 7,
+    frequency: 2349.32,
+    wavelength: 14.69
+}, {
+    name: 'D#',
+    altname: 'Eb',
+    position: 7,
+    frequency: 2489.02,
+    wavelength: 13.86
+}, {
+    name: 'E',
+    altname: null,
+    position: 7,
+    frequency: 2637.02,
+    wavelength: 13.08
+}, {
+    name: 'F',
+    altname: null,
+    position: 7,
+    frequency: 2793.83,
+    wavelength: 12.35
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 7,
+    frequency: 2959.96,
+    wavelength: 11.66
+}, {
+    name: 'G',
+    altname: null,
+    position: 7,
+    frequency: 3135.96,
+    wavelength: 11.00
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 7,
+    frequency: 3322.44,
+    wavelength: 10.38
+}, {
+    name: 'A',
+    altname: null,
+    position: 7,
+    frequency: 3520.00,
+    wavelength: 9.80
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 7,
+    frequency: 3729.31,
+    wavelength: 9.25
+}, {
+    name: 'B',
+    altname: null,
+    position: 7,
+    frequency: 3951.07,
+    wavelength: 8.73
+}, {
+    name: 'C',
+    altname: null,
+    position: 8,
+    frequency: 4186.01,
+    wavelength: 8.24
+}, {
+    name: 'C#',
+    altname: 'Db',
+    position: 8,
+    frequency: 4434.92,
+    wavelength: 7.78
+}, {
+    name: 'D',
+    altname: null,
+    position: 8,
+    frequency: 4698.63,
+    wavelength: 7.34
+}, {
+    name: 'D#',
+    altname: 'Eb',
+    position: 8,
+    frequency: 4978.03,
+    wavelength: 6.93
+}, {
+    name: 'E',
+    altname: null,
+    position: 8,
+    frequency: 5274.04,
+    wavelength: 6.54
+}, {
+    name: 'F',
+    altname: null,
+    position: 8,
+    frequency: 5587.65,
+    wavelength: 6.17
+}, {
+    name: 'F#',
+    altname: 'Gb',
+    position: 8,
+    frequency: 5919.91,
+    wavelength: 5.83
+}, {
+    name: 'G',
+    altname: null,
+    position: 8,
+    frequency: 6271.93,
+    wavelength: 5.50
+}, {
+    name: 'G#',
+    altname: 'Ab',
+    position: 8,
+    frequency: 6644.88,
+    wavelength: 5.19
+}, {
+    name: 'A',
+    altname: null,
+    position: 8,
+    frequency: 7040.00,
+    wavelength: 4.90
+}, {
+    name: 'A#',
+    altname: 'Bb',
+    position: 8,
+    frequency: 7458.62,
+    wavelength: 4.63
+}, {
+    name: 'B',
+    altname: null,
+    position: 8,
+    frequency: 7902.13,
+    wavelength: 4.37
+}];
+var sound  = function () {
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    return {
+        note : function (freq) {
+            var oscillator = audioCtx.createOscillator();
+            var gainNode = audioCtx.createGain();
+            oscillator.connect(gainNode);
+            gainNode.connect(audioCtx.destination);
+            oscillator.type = 'sine'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+            oscillator.frequency.value = freq; // value in hertz
+            oscillator.start();
+            oscillator.stop(audioCtx.currentTime + 0.5);
+        }
+}
+}();
+$( document ).ready(function() {
+    $('#play').click(function () {
+        sound.note(440);
+    });
+    notes.forEach(function(item){
+        var keyid = item.name.replace('#',"_")+item.position;
+        if(item.name.indexOf('#') === -1) {
+            //white keys
+            $('.piano').append("<li class='white' id='" + keyid + "'></li>");
+        }else{
+            //black keys
+            $('.piano').append("<li class='black' id='" + keyid + "'></li>");
+        }
+        $('#'+keyid).click(function () {
+            sound.note(item.frequency);
+        })
+    })
+});
